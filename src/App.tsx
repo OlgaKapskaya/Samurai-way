d8faedbb8b98779fd9fcaa6d001026aa1582b4cd
OlgaKapskaya <olikbuko@gmail.com>
2022-09-08 14:05:12 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import './components/header.tsx'
+import Header from "./components/header";
+import Technologies from "./components/technologies";
+
+function App() {
+  return (
+    <div >
+        Let's go!
+        <Header />
+        <Technologies />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/technologies.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Tecnologies() {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+export default Tecnologies;
