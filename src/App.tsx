20568617732f4a7ddd8b460e21d2eda76adb65ea
OlgaKapskaya <olikbuko@gmail.com>
2022-09-08 15:41:34 +0300

    les 11 08.09.2022

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,29 @@ function App() {
         </header>
 
         <nav className='nav'>
-            <div>Profile</div>
-            <div>Messages</div>
-            <div>News</div>
-            <div>Music</div>
-            <div>Settings</div>
+            <div><a>Profile</a></div>
+            <div><a>Messages</a></div>
+            <div><a>News</a></div>
+            <div><a>Music</a></div>
+            <div><a>Settings</a></div>
         </nav>
-        <div className='content'>Main content</div>
+        <div className='content'>
+            <div>
+                <img src='https://html5css.ru/css/img_forest.jpg'/>
+            </div>
+            <div>
+                <img src={'https://img.freepik.com/free-icon/jpg-extension_318-1831.jpg?w=360'}/>
+                description
+            </div>
+            <div>
+                My post
+                <div>New post</div>
+
+            </div>
+            <div>
+                Post
+            </div>
+        </div>
         <footer className='footer'>footer</footer>
     </div>
   );
