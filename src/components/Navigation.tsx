1def5db5cf400004cd8c1753d6ab83970132cfe5
OlgaKapskaya <olikbuko@gmail.com>
2022-09-15 15:29:29 +0300

    edit .css to .module.css

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import "../CSS-modules/Navigation.css"
+import nav from "../CSS-modules/Navigation.module.css"
+import messages from "./icons/messages.png"
+import news from "./icons/news.png"
+import profile from "./icons/profile.png"
+import music from "./icons/music.png"
+import settings from "./icons/settings.png"
+
 
 function Navigation(){
     return (
-        <nav className='nav'>
-            <div><a href={"#"}>Profile</a></div>
-            <div><a href={"#"}>Messages</a></div>
-            <div><a href={"#"}>News</a></div>
-            <div><a href={"#"}>Music</a></div>
-            <div><a href={"#"}>Settings</a></div>
+        <nav className={nav.nav}>
+            <div className={nav.item}><img className={nav.icons} src={profile}/><a className={nav.link} href={"#"}> Profile</a></div>
+            <div className={nav.item}><img className={nav.icons} src={messages}/><a className={nav.link} href={"#"}> Messages</a></div>
+            <div className={nav.item}><img className={nav.icons} src={news}/><a className={nav.link} href={"#"}> News</a></div>
+            <div className={nav.item}><img className={nav.icons} src={music}/><a className={nav.link} href={"#"}> Music</a></div>
+            <div className={nav.item}><img className={nav.icons} src={settings}/><a className={nav.link} href={"#"}> Settings</a></div>
         </nav>
     )
 }
