073b6601cadee2dd7b4a3cd30b0daf4ef313d96e
OlgaKapskaya <olikbuko@gmail.com>
2022-09-25 13:09:09 +0300

    Split the dialogs component into 2 smaller components

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,18 @@ import s from "./Dialogs.module.css"
 
 export const Dialogs = () => {
     return (
-    <div className={"content"}>
-        Dialogs
-    </div>
+        <div className={"content"}>
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    Names
+                </div>
+                <div className={s.dialogsMessages}>
+                    <div className={s.message}>Message 1</div>
+                    <div className={s.message}>Message 2</div>
+                    <div className={s.message}>Message 3</div>
+                    <div className={s.message}>Message 4</div>
+                </div>
+            </div>
+        </div>
     )
 }
