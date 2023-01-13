044ae4ca892329a6287007cad948603c5fa98c2f
OlgaKapskaya <olikbuko@gmail.com>
2023-01-13 08:33:05 +0300

    chrome extensions для react и redux (lesson 91)

--- src/bll/redux-store.ts
+++ src/bll/redux-store.ts
@@ -1,14 +1,13 @@
-import {applyMiddleware, combineReducers, legacy_createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, legacy_createStore} from "redux";
 import {profileReducer} from "./reducers/profileReducer";
 import {dialogsReducer} from "./reducers/dialogsReducer";
 import {usersReducer} from "./reducers/usersReducer";
 import {authReducer} from "./reducers/authReducer";
 import thunkMiddleware, {ThunkDispatch} from "redux-thunk";
-import { reducer as formReducer } from "redux-form"
+import {reducer as formReducer} from "redux-form"
 import {ActionDispatchType} from "./store";
 import {useDispatch} from "react-redux";
 import {appReducer} from "./reducers/appReducer";
-
 
 
 let rootReducer = combineReducers({
@@ -19,7 +18,11 @@ let rootReducer = combineReducers({
     app: appReducer,
     form: formReducer
 })
-export let store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware));
+//@ts-ignore
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = legacy_createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunkMiddleware)
+));
 
 export type stateType = ReturnType<typeof rootReducer> // типизация того,что наш стор вернет
 export type storeType = typeof store // типизация самого стора
